a36d5084fd499afb1af87c9cf54ca5d4f84a8ad5
HybridDog <adresse_test@t-online.de>
2013-05-18 20:22:59 +0200

    update

--- init.lua
+++ init.lua
@@ -28,7 +28,7 @@ minetest.register_node("lavacooling:basalt", {
 	description = "Basalt",
 	tiles = {"lavacooling_basalt.png","lavacooling_basalt.png","lavacooling_basalt_side.png"},
 	sounds = default.node_sound_stone_defaults(),
-	groups = {cracky=1,level=2},
+	groups = {cracky=3},
 	drop = "default:cobble",
 })
 
@@ -48,6 +48,14 @@ minetest.register_craft({
 
 
 --ABMs
+
+local function ore()
+	if math.random(50) == 1 then return "default:stone_with_coal" end
+	if math.random(200) == 1 then return "default:stone_with_iron" end
+	if math.random(500) == 1 then return "default:stone_with_diamond" end
+	if math.random(600) == 1 then return "default:stone_with_mese" end
+	return "default:stone"
+end
 
 local function lavacooling_abm(input, coolingnodes, output)
 minetest.register_abm ({
@@ -83,7 +91,7 @@ minetest.register_abm ({
 	action = function (pos)
 		for _, lava in ipairs(LAVA) do
 			if minetest.env: get_node({x=pos.x, y=pos.y+1, z=pos.z}).name == lava then
-				minetest.env: add_node (pos, {name = "default:stone"})
+				minetest.env: add_node (pos, {name = ore()})
 				minetest.sound_play("lavacooling", {pos = pos,	gain = 0.5,	max_hear_distance = 5})
 				return
 			end
@@ -91,12 +99,83 @@ minetest.register_abm ({
 	end,
 })
 
---[[
+if minetest.get_modpath("sumpf") then
+lavacooling_abm("default:lava_source", {"sumpf:dirtywater_flowing", "sumpf:dirtywater_source"}, "default:obsidian")
+
+local function dirtyblocks(pos)
+	if minetest.env: get_node({x=pos.x, y=pos.y-1, z=pos.z}).name == "sumpf:dirtywater_flowing" then
+		return "default:dirt"
+	end
+	for i=-1,1,2 do
+		if minetest.env: get_node({x=pos.x+i, y=pos.y, z=pos.z}).name == "sumpf:dirtywater_flowing"
+		or minetest.env: get_node({x=pos.x, y=pos.y, z=pos.z+i}).name == "sumpf:dirtywater_flowing"
+			then return "default:sand"
+		end
+	end
+	if minetest.env: get_node({x=pos.x, y=pos.y-1, z=pos.z}).name == "sumpf:dirtywater_source" then
+		if math.random(37) == 1 then return "sumpf:kohle" end
+		if math.random(50) == 1 then return "sumpf:eisen" end
+		if math.random(200) == 1 then return "default:mese" end
+		if math.random(250) == 1 then return "default:obsidian" end
+		return "sumpf:junglestone"
+	end
+end
+
+local function dirtyblocks2(pos)
+	if minetest.env: get_node({x=pos.x, y=pos.y-1, z=pos.z}).name == "default:lava_flowing" then
+		return "default:clay"
+	end
+	for i=-1,1,2 do
+		if minetest.env: get_node({x=pos.x+i, y=pos.y, z=pos.z}).name == "default:lava_flowing"
+			then return "sumpf:sumpf"
+		end
+		if minetest.env: get_node({x=pos.x, y=pos.y, z=pos.z+i}).name == "default:lava_flowing"
+			then return "sumpf:peat"
+		end
+	end
+end
+
+local function dirtyblocks3(pos)
+	if minetest.env: get_node({x=pos.x, y=pos.y-1, z=pos.z}).name == "default:lava_flowing" then
+		return "default:gravel"
+	end
+end
+minetest.register_abm ({
+	nodenames = {"sumpf:dirtywater_flowing"},
+	interval = 0,
+	chance = 1,
+	action = function (pos)
+		local nam = dirtyblocks3(pos)
+		if nam then
+			minetest.env: add_node (pos, {name = nam})
+			minetest.sound_play("lavacooling", {pos = pos,	gain = 0.5,	max_hear_distance = 5})
+		end
+	end,
+})
+minetest.register_abm ({
+	nodenames = {"sumpf:dirtywater_source"},
+	interval = 0,
+	chance = 1,
+	action = function (pos)
+		local nam = dirtyblocks2(pos)
+		if nam then
+			minetest.env: add_node (pos, {name = nam})
+			minetest.sound_play("lavacooling", {pos = pos,	gain = 0.5,	max_hear_distance = 5})
+		end
+	end,
+})
+
 minetest.register_abm ({
 	nodenames = {"default:lava_flowing"},
-	interval = 5,
-	chance = 60,
+	interval = 0,
+	chance = 1,
 	action = function (pos)
-		minetest.env: add_node (pos, {name = "default:obsidian"})
+		local nam = dirtyblocks(pos)
+		if nam then
+			minetest.env: add_node (pos, {name = nam})
+			minetest.sound_play("lavacooling", {pos = pos,	gain = 0.5,	max_hear_distance = 5})
+		end
 	end,
-})]]
+})
+
+end
